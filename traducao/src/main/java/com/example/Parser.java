b9dae9f83a3db57d6b7197af6a1b76b915a040db
thalesgmendes <thalesmendes551@gmail.com>
2024-11-21 10:57:11 -0300

    adicionando Scanner

--- traducao/src/main/java/com/example/Parser.java
+++ traducao/src/main/java/com/example/Parser.java
@@ -1,27 +1,26 @@
 package com.example;
 
 public class Parser {
-    private byte[] input;
-    private int current; 
+    private Scanner scan;
+    private char currentToken;
 
-	public Parser (byte[] input) {
-        this.input = input;
+    public Parser(byte[] input) {
+        scan = new Scanner(input);
+        currentToken = scan.nextToken();
     }
 
     public void parse () {
         expr();
     }
 
-    private char peek () {
-        if (current < input.length)
-           return (char)input[current];
-       return '\0';
+    private void nextToken () {
+        currentToken = scan.nextToken();
     }
 
-    private void match (char c) {
-        if (c == peek()) {
-            current++;
-        } else {
+    private void match (char t) {
+        if (currentToken == t) {
+            nextToken();
+        }else {
             throw new Error("syntax error");
         }
     }
@@ -32,26 +31,25 @@ public class Parser {
     }
 
     void digit () {
-        if (Character.isDigit(peek())) {
-            System.out.println("push " + peek());
-            match(peek());
+        if (Character.isDigit(currentToken)) {
+						System.out.println("push " + currentToken);
+            match(currentToken);
         } else {
            throw new Error("syntax error");
         }
     }
 
     void oper () {
-        if (peek() == '+') {
+        if (currentToken == '+') {
             match('+');
             digit();
             System.out.println("add");
             oper();
-        } else if (peek() == '-') {
+        } else if (currentToken == '-') {
             match('-');
             digit();
             System.out.println("sub");
             oper();
-        } 
+        }
     }
-
 }
--- traducao/src/main/java/com/example/Scanner.java
+++ traducao/src/main/java/com/example/Scanner.java
@@ -1,5 +1,43 @@
 package com.example;
 
 public class Scanner {
-    
+    private byte[] input;
+    private int current; 
+
+	public Scanner (byte[] input) {
+        this.input = input;
+    }
+
+    private char peek () {
+        if (current < input.length)
+           return (char)input[current];
+       return '\0';
+    }
+
+    private void advance()  {
+        char ch = peek();
+        if (ch != '\0') {
+            current++;
+        }
+    }
+
+    public char nextToken () {
+        char ch = peek();
+
+        if (Character.isDigit(ch)) {
+						advance();
+            return ch;
+        }
+
+        switch (ch) {
+            case '+':
+            case '-':
+                advance();
+                return ch;
+            default:
+                break;
+        }
+
+        return '\0';
+    }
 }
